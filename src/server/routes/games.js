af6a46750d9095fc782419d017cdcf1741b3cabb
Timothy Chew <timothyrchew@gmail.com>
2016-11-14 12:35:26 -0600

    annotate route files

--- src/server/routes/games.js
+++ src/server/routes/games.js
@@ -6,13 +6,19 @@ const environment = process.env.NODE_ENV || "development";
 const knexConfig = require('../../../knexfile')[environment];
 const knex = require('knex')(knexConfig);
 
+//=-=-=-==-=-=-=- ROUTE PREFIX -- '/users/:user_id/games/:game_id/' =-=-=-=-==---
 
 //create a new game, designate creator as the admin
+//--make entry into games table
+//--create entries into game_websites for each designated URL
 router.post('/new', function(req, res){
 
 });
 
 //add user to an existing game
+//--register player in game_player table
+//--fetch id for game_websites (for tracking this players score for this game)
+//--create entry in player_game_website for each ID above
 router.post('/join', function(req, res){
 
 });
@@ -22,6 +28,10 @@ router.patch('/:game_id', function(req, res){
 
 });
 
+//fetch details about all games that user belongs to
+router.get('/', function(req, res){
+
+});
 //fetch details about a single game
 router.get('/:game_id', function(req, res){
 
--- src/server/routes/time-tracking.js
+++ src/server/routes/time-tracking.js
@@ -7,15 +7,24 @@ const knexConfig = require('../../../knexfile')[environment];
 const knex = require('knex')(knexConfig);
 
 
+//=-=-=-==-=-=-=- ROUTE PREFIX -- '/users/:user_id/time/' =-=-=-=-==---
+
+
 //post URL and time data to users account
-//check 'games_table' to see what games this user belongs to
-//for each game, see what URLs need to be tracked
-//if there is a matching ULR in the log, increment their total time based off the new data
+//--check 'games_table' and fetch all the games this user belongs to
+//--for each game, fetch game_website entry
+//--for each game_website, fetch player_game_website
+//--for each player_game_website, see the log contains a matching URL, and increment the time according to the posted data
 router.post('/log', function(req, res){
   res.send("boom");
 });
 
 //fetch URL and time data for user
+//--create games array that will contian user score for each game they belong to
+//--check 'games_table' and fetch all the games this user belongs to
+//--for each game, push {game_id} into games array and fetch game_website entry
+//--for each game_website, fetch player_game_website
+//--attach URL and player score to game object
 router.post('/get', function(req, res){
 
 });
