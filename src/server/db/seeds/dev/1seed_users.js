adaa3ae8afa11959223d130eefff38ca7858193c
Robert Varela <rvarela@utexas.edu>
2016-11-15 16:28:59 -0600

    created form for new game

--- src/server/db/seeds/dev/1seed_users.js
+++ src/server/db/seeds/dev/1seed_users.js
@@ -1,29 +1,29 @@
-const bcrypt=require('bcrypt');
+const bcrypt = require('bcrypt');
 const saltRounds = 4;
-const passwordHash = bcrypt.hashSync( 'password1', saltRounds );
+const passwordHash = bcrypt.hashSync('password1', saltRounds);
 
-exports.seed = function( knex, Promise ) {
-    // Deletes ALL existing entries
-    return knex( 'players' ).del()
-        .then( function() {
-            return Promise.all( [
-                // Inserts seed entries
-                knex( 'players' ).insert( {
-                    id: 1,
-                    first_name: "Tim",
-                    last_name: "Chew",
-                    username: "tryanc",
-                    password: passwordHash,
-                    email: 'timothyrchew@gmail.com'
-                } ),
-                knex( 'players' ).insert( {
-                    id:2,
-                    first_name: "Jeannie",
-                    last_name: "Y",
-                    username: "jeansey",
-                    password: passwordHash,
-                    email: 'jeanbean@gmail.com'
-                } ),
-            ] );
-        } );
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('players').del()
+    .then(function() {
+      return Promise.all([
+        // Inserts seed entries
+        knex('players').insert({
+          id: 1,
+          first_name: "Tim",
+          last_name: "Chew",
+          username: "tryanc",
+          password: passwordHash,
+          email: 'timothyrchew@gmail.com'
+        }),
+        knex('players').insert({
+          id: 2,
+          first_name: "Jeannie",
+          last_name: "Y",
+          username: "jeansey",
+          password: passwordHash,
+          email: 'jeanbean@gmail.com'
+        }),
+      ]);
+    });
 };
