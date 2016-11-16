3611d3bed878151f6333d45d63432acdd58f5622
Robert Varela <rvarela@utexas.edu>
2016-11-16 17:42:24 -0600

    leaderboard charts complete. Need info.

--- src/public/js/controllers/leaderboardController.js
+++ src/public/js/controllers/leaderboardController.js
@@ -1,4 +1,3 @@
 app.controller('leaderboardController', ['$scope', '$http', '$location',
-  'currentUser',
-  function($scope, $http, $location, currentUser) {}
+  function($scope, $http, $location) {}
 ]);
