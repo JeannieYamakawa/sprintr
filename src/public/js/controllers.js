ff1141e00782706b9ea15186714382c5efb92f4b
Timothy Chew <timothyrchew@gmail.com>
2016-11-16 14:32:45 -0600

    boom

--- src/public/js/controllers.js
+++ src/public/js/controllers.js
@@ -19,33 +19,36 @@ app.controller('homeController', ['$scope', '$http', '$location', '$window',
 ]);
 
 app.controller('signupController', ['$scope', '$http', '$location',
-  '$window',
-  function($scope, $http, $location, $window) {
+            '$window',
+            function($scope, $http, $location, $window) {
 
-    $scope.view = {};
-    $scope.view.signupFormInfo = {
-      firstname: "",
-      lastname: "",
-      username: "",
-      password: "",
-      email: ""
-    }
+                $scope.view = {};
+                $scope.view.signupFormInfo = {
+                    firstname: "",
+                    lastname: "",
+                    username: "",
+                    password: "",
+                    email: ""
+                }
 
-    $scope.submitSignupForm = function(event) {
-      event.preventDefault();
+                $scope.submitSignupForm = function(event) {
+                    event.preventDefault();
 
-      console.log($scope.view.signupFormInfo,
-        'signupFormInfo from controller');
+                    console.log($scope.view.signupFormInfo,
+                        'signupFormInfo from controller');
 
-      $http.post('/signup', $scope.view.signupFormInfo)
-        .then(function(response) {
-          console.log(response, 'response in dot then function');
-          $window.localStorage.setItem('token', response.data.token);
-          var userId = response.data.id;
-          $location.path('/dashboard')
-        })
-    }
-] );
+                    $http.post('/signup', $scope.view.signupFormInfo)
+                        .then(function(response) {
+                            console.log(response, 'response in dot then function');
+                            $window.localStorage.setItem('token', response.data.token);
+                            var userId = response.data.id;
+                            $location.path('/dashboard')
+                        })
+                }
+              }
+            ]);
+
+
 
 app.controller('dashboardController', ['$scope', '$http', '$location',
     '$window', 'currentUser', '$routeParams',
@@ -62,7 +65,6 @@ app.controller('dashboardController', ['$scope', '$http', '$location',
             $http.get('users/' + currentUser.id + '/games').then(function(
                 response) {
                 var gamesList = response.data;
-                console.log("gamesList", gamesList);
 
                 var promiseArray = [];
 
@@ -76,9 +78,12 @@ app.controller('dashboardController', ['$scope', '$http', '$location',
                   }));
                 })
 
-                Promise.all(promiseArray).then(function(data){
-                  console.log(data);
-                })
+                Promise.all(promiseArray).then(function(games){
+                  console.log("gamesList", gamesList);
+                  games.forEach(function(game){
+                    console.log("game details", game);
+                  });
+                });
 
                 $scope.view.games = response.data;
             });
@@ -98,44 +103,6 @@ app.controller('dashboardController', ['$scope', '$http', '$location',
   }
 ]);
 
-app.controller('dashboardController', ['$scope', '$http', '$location',
-  '$window', 'currentUser', '$routeParams',
-  function($scope, $http, $location, $window, currentUser, $routeParams) {
-
-    $scope.view = {};
-    $scope.currentUser = currentUser;
-    console.log($scope.currentUser, 'scope.currentUser');
-
-    // only render this part of the page if the user is using a valid token
-    if ($scope.currentUser) {
-      console.log($scope.currentUser, '$scope.currentUser');
-      $scope.view.hi = "You have a valid token";
-      $http.get('users/' + $scope.currentUser.id + '/games').then(function(
-        response) {
-        console.log(response.data,
-          'response.data from controller dashboard after knex calls')
-        $scope.view.games = response.data;
-      });
-
-    } else {
-      $location.path('/')
-    }
-]);
-
-
-    $scope.singleGameClicked = function(gameId) {
-      console.log(gameId, 'gameId from single clicked game funct');
-      $http.get('users/' + $scope.currentUser.id + '/games/' + gameId).then(
-        function(response) {
-          console.log(response,
-            'response from singleGameClicked function in dash controller'
-          );
-          //this will be set to go to the view single game leaderboard page.
-          // $location.path('/')
-        })
-    }
-  }
-]);
 
 app.controller('loginController', ['$scope', '$http', '$location',
   '$window',
