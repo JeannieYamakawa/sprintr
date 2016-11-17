90ce9306c2340e9b8e8f827f45e646ecf64c963d
Timothy Chew <timothyrchew@gmail.com>
2016-11-17 10:39:52 -0600

    structuring leaderboard data for donut chart

--- src/public/js/controllers/leaderboardController.js
+++ src/public/js/controllers/leaderboardController.js
@@ -7,6 +7,7 @@ app.controller('leaderboardController', ['$scope', '$http', '$location', 'curren
 
     $http.get('users/' + currentUser.id + '/games/' + gameId).then(function(game) {
         var gameDetails = game.data;
+        console.log(gameDetails);
         var playerTotalTimes = [];
 
 
@@ -75,15 +76,58 @@ app.controller('leaderboardController', ['$scope', '$http', '$location', 'curren
             backgroundColor.push(color);
         }
 
-        console.log(chartLables);
-        console.log(chartData);
-        console.log(backgroundColor);
-        console.log(borderColor);
+        // console.log(chartLables);
+        // console.log(chartData);
+        // console.log(backgroundColor);
+        // console.log(borderColor);
 
 
-        //Strutre data for player stats chart
+        //Structure data for player stats donut chart
 
+        var currentUserStats;
 
+        gameDetails.game_stats.forEach(function(player){
+          if (player.player_id === currentUser.id){
+            currentUserStats = player;
+          }
+        })
+
+        var donutHoverOptions = [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ];
+
+        var donutColorOptions = [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ];
+
+        var donutLabels = [];
+        var donutData = [];
+        var donutBackground = [];
+        var donutHover = [];
+
+        currentUserStats.stats.forEach(function(url){
+          donutLabels.push(url.domain);
+          donutData.push(url.total_time);
+          var hover = donutHoverOptions.splice(0, 1)[0];
+          donutHover.push(hover);
+          var color = donutColorOptions.splice(0, 1)[0];
+          donutBackground.push(color);
+        });
+
+        console.log(donutLabels);
+        console.log(donutData);
+        console.log(donutBackground);
+        console.log(donutHover);
 
     })
 }]);
