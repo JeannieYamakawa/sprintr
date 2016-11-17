3735670c6ded4943a9798b8cb83405fec1a2c503
Robert Varela <rvarela@utexas.edu>
2016-11-17 10:43:49 -0600

    add css to confirm join html

--- src/public/js/controllers/confirmJoinController.js
+++ src/public/js/controllers/confirmJoinController.js
@@ -1,23 +1,29 @@
-app.controller('confirmJoinController', ['$scope', '$http', '$location', '$window','currentUser', 'currentGame', function($scope, $http, $location, $window, currentUser, currentGame){
+app.controller('confirmJoinController', ['$scope', '$http', '$location',
+  '$window', 'currentUser', 'currentGame',
+  function($scope, $http, $location, $window, currentUser, currentGame) {
     $scope.currentUser = currentUser;
     $scope.currentGame = {};
-    $scope.confirmForm={};
+    $scope.confirmForm = {};
 
     // console.log($scope.currentUser.id, 'scope.currentUser in joinGameController');
     $scope.currentGame.name = $window.localStorage.getItem('game');
     $scope.currentGame.gameId = $window.localStorage.getItem('gameId');
-    $scope.currentGame.stats = JSON.parse($window.localStorage.getItem('gameStats'));
+    $scope.currentGame.stats = JSON.parse($window.localStorage.getItem(
+      'gameStats'));
     $scope.currentGame.startTime = $window.localStorage.getItem('startTime');
     $scope.currentGame.sites = $scope.currentGame.stats[0];
     $scope.currentGame.gameType = $window.localStorage.getItem('gameType');
-    console.log($scope.currentGame.gameType, 'game type !!');
+    console.log($scope.currentGame);
+    // console.log($scope.currentGame.gameType, 'game type !!');
 
-    $scope.submitJoinClicked = function(event, formInfo){
-        event.preventDefault();
-        // console.log(formInfo);
-        $http.post('/users/'+ $scope.currentUser.id +'/games/'+ formInfo.gameId +'/join', formInfo).then(function(response){
-            $location.path('/dashboard')
-        })
+    $scope.submitJoinClicked = function(event, formInfo) {
+      event.preventDefault();
+      // console.log(formInfo);
+      $http.post('/users/' + $scope.currentUser.id + '/games/' + formInfo
+        .gameId + '/join', formInfo).then(function(response) {
+        $location.path('/dashboard')
+      })
     }
 
-}])
+  }
+])
