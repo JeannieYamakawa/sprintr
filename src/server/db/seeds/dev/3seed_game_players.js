311885b92bf24a2503732c286730e3fe364bf5e1
Timothy Chew <timothyrchew@gmail.com>
2016-11-18 12:18:07 -0600

    rework seed files

--- src/server/db/seeds/dev/3seed_game_players.js
+++ src/server/db/seeds/dev/3seed_game_players.js
@@ -5,6 +5,7 @@ exports.seed = function(knex, Promise) {
             return Promise.all([
                 // Inserts seed entries
                 knex.raw('ALTER SEQUENCE game_player_id_seq RESTART WITH 1'),
+                ///LEARNING REACT GAME
                 knex('game_player').insert({
                     game_id: '1',
                     player_id: '1',
@@ -25,6 +26,7 @@ exports.seed = function(knex, Promise) {
                     player_id: '4',
                     final_ranking: null
                 }),
+                //REAL ESTATE GAME
                 knex('game_player').insert({
                     game_id: '2',
                     player_id: '1',
@@ -45,6 +47,7 @@ exports.seed = function(knex, Promise) {
                     player_id: '4',
                     final_ranking: null
                 }),
+                //FANTAST FOOTBALL GAME
                 knex('game_player').insert({
                     game_id: '3',
                     player_id: '1',
@@ -65,9 +68,25 @@ exports.seed = function(knex, Promise) {
                     player_id: '4',
                     final_ranking: null
                 }),
+                //ALAPCA BREEDING CLUB
                 knex('game_player').insert({
                     game_id: '4',
                     player_id: '1',
+                    final_ranking: null
+                }),
+                knex('game_player').insert({
+                    game_id: '5',
+                    player_id: '2',
+                    final_ranking: null
+                }),
+                knex('game_player').insert({
+                    game_id: '5',
+                    player_id: '3',
+                    final_ranking: null
+                }),
+                knex('game_player').insert({
+                    game_id: '5',
+                    player_id: '4',
                     final_ranking: null
                 }),
             ]);
