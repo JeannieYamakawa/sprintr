e6ce9ee15560b25a950b6239f6eecc676f44b38f
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-11-17 18:43:37 -0600

    updates new game form to stop deleting sites with enter

--- src/public/js/controllers/findGameController.js
+++ src/public/js/controllers/findGameController.js
@@ -2,6 +2,8 @@ app.controller('findGameController', ['$scope', '$http', '$location', '$window',
     $scope.currentUser = currentUser;
     // console.log($scope.currentUser.id, 'scope.currentUser in joinGameController');
     $scope.logout = logoutServ.logout;
+    console.log($scope.currentUser.id, 'current user id in confirm controller');
+
 
 
     $scope.cancelClicked = function(event){
@@ -12,7 +14,7 @@ app.controller('findGameController', ['$scope', '$http', '$location', '$window',
 
     $scope.goToConfirm = function(event, gameInfo){
         event.preventDefault();
-        // console.log(gameInfo, 'gameInfo', $scope.currentUser.id);
+        console.log(gameInfo, 'gameInfo');
         $http.get('/users/'+ $scope.currentUser.id +'/games/'+ gameInfo.gameId).then(function(response){
             console.log(response.data, 'Dot then function inside goToConfirm inside joinGameController');
             // currentGame.setWholeGame(response.data.name);
