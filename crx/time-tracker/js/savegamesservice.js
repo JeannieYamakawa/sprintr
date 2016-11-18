a506c4259e1d54baac77c653486c9e12fff55dc8
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-11-18 00:11:59 -0600

    hooks up chart.js to extension for single game view

--- crx/time-tracker/js/savegamesservice.js
+++ crx/time-tracker/js/savegamesservice.js
@@ -1,22 +1,32 @@
-app.service('saveGames', function() {
+app.service( 'saveGames', function() {
 
     this.allGames = [];
     this.currentGame = {};
+    this.currentUser = {};
 
-    this.setAllGames = function(games) {
-            this.allGames = games;
-            console.log(this.allGames, 'setAllGames log');
-        };
-    this.getAllGames = function() {
-            console.log(this.allGames, 'GET THE DAMN GAMES log');
-            return this.allGames;
-        };
-    this.setCurrentGameId = function(gameId){
-        this.currentGame.id = gameId;
-        console.log(this.currentGame, 'this.currentGame in service SET');
+    this.setAllGames = function( games ) {
+        this.allGames = games;
+        console.log( this.allGames, 'setAllGames log' );
     };
-    this.getCurrentGameId = function(){
-        console.log(this.currentGame, 'this.currentGame in service GET');
+    this.getAllGames = function() {
+        console.log( this.allGames, 'GET THE DAMN GAMES log' );
+        return this.allGames;
+    };
+    this.setCurrentGameId = function( gameId ) {
+        this.currentGame.id = gameId;
+        console.log( this.currentGame, 'this.currentGame in service SET' );
+    };
+    this.getCurrentGameId = function() {
+        console.log( this.currentGame, 'this.currentGame in service GET' );
         return this.currentGame;
+    };
+    this.setCurrentUser = function( user ) {
+        this.currentUser = user;
+        console.log( this.currentGame, 'this.currentGame in service SET' );
+
+    };
+    this.getCurrentUser = function() {
+        console.log( this.currentGame, 'this.currentUser in service GET' );
+        return this.currentUser;
     }
-});
+} );
