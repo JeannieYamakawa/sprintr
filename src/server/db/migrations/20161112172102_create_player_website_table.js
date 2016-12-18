79d79de7997c71f1f1e0df4cf69afc992d48560f
Timothy Chew <timothyrchew@gmail.com>
2016-12-17 18:26:22 -0600

    fix reference in migration table

--- src/server/db/migrations/20161112172102_create_player_website_table.js
+++ src/server/db/migrations/20161112172102_create_player_website_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('player_websites', function(table) {
     table.increments();
     table.integer('game_id').unsigned().index().references('id').inTable('games').onDelete('CASCADE');
-    table.integer('player_id').unsigned().index().references('id').inTable('users').onDelete('CASCADE');
+    table.integer('player_id').unsigned().index().references('id').inTable('players').onDelete('CASCADE');
     table.integer('website_id').unsigned().index().references('id').inTable('websites').onDelete('CASCADE');
     table.integer('total_time');
   });
